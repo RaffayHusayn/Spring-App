b4f0ac73911c6236c927d66a16b604e6784e9f09
Raffay Syed <raffayhusayn@gmail.com>
2022-03-05 23:06:17 -0500

    Fixing @PropertySource bug

--- src/main/java/com/fclass/AppConfig.java
+++ src/main/java/com/fclass/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan("com.fclass")
-@PropertySource("classpath: application.properties") // this is required for @Value("${properties value}")
+@PropertySource("classpath:application.properties") // this is required for @Value("${properties value}")
 @Import({SecondConfig.class, App.class})
 public class AppConfig {
 
